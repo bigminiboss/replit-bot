ed75d35ddd95ddac8ef11bbaec53ff6fa0b00387
Firepup650 <74429872+F1repup650@users.noreply.github.com>
2023-04-20 18:26:30 -0500

    Drill.
    Capitalization, and `nto` to `not`.

--- replit_bot/exceptions.py
+++ replit_bot/exceptions.py
@@ -1,17 +1,17 @@
-"""file that stores exceptions"""
+"""File that stores exceptions"""
 
 
 class NonRequiredParamsMustHaveDefault(Exception):
-    """for when the user does nto specify default for a non required parameter"""
+    """For when the user does not specify default for a non required parameter"""
 
 
 class InvalidSid(Exception):
-    """sid invalid"""
+    """SID invalid"""
 
 
 class NamesMustBeAlphanumeric(Exception):
-    """for when command name is not alphanumeric"""
+    """For when command name is not alphanumeric"""
 
 
 class MustBeRunOnReplitForButtons(Exception):
-    """you must be on replit to run bot for now"""
+    """You must be on replit to run bot for now, if you want buttons"""
